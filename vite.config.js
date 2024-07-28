f0235c7e071918cb30242a9aea7f88f86d23db1e
Nathan Thai <n3cr0d3m0nncrdmn@gmail.com>
2024-07-28 14:21:51 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            
+          },
+      },
+  },
 })
