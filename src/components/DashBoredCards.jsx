554ab093aa6059665b784e5fce9190843f812a8c
NathanThai2201 <thainhatanh2201@gmail.com>
2025-02-22 23:55:00 -0700

    up

--- src/components/DashBoredCards.jsx
+++ src/components/DashBoredCards.jsx
@@ -62,7 +62,7 @@ export const DashBoredCards = () => {
     
     const generateRandomCard = async () => {
         // amounts: rarity from 1 -> 5
-        let amounts = [9,9,8,6,5]
+        let amounts = [10,10,8,7,5]
         let randomCardNumber;
         const randomRarityNumber = Math.floor(Math.random() * 10000) + 1;
         let rarity;
