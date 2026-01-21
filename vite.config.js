dbac71029b0b20075c8d0524888d0bec8df0b113
NathanThai2201 <thainhatanh2201@gmail.com>
2026-01-20 23:28:09 -0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,44 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My App',
+        short_name: 'MyApp',
+        description: 'My React PWA',
+        theme_color: '#000000',
+        background_color: '#000000',
+
+        // IMPORTANT FOR SAFARI
+        display: 'standalone',
+
+        start_url: '/',
+        scope: '/',
+
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: "/",
-  plugins: [react()],
   build: {
     rollupOptions: {
         input: {
