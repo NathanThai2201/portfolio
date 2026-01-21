d978fa0bf0e1e2d74138570221569b32328ffad9
NathanThai2201 <thainhatanh2201@gmail.com>
2026-01-21 05:54:04 -0700

    up

--- src/pages/VexCalculator.jsx
+++ src/pages/VexCalculator.jsx
@@ -153,7 +153,7 @@ export function VexCalculator() {
     // Check if user multiplied by 69420
     if (display.match(/×69420$/)) {
       setExpression(display);
-      setDisplay("972864627325e20");
+      setDisplay("972864627325e27");
       setJustEvaluated(true);
       return;
     }
